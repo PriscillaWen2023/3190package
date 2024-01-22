a0bc8b5400c9bba79e7e26b7a94d93be8562c0af
Priscilla Wen <sunnywen098@gmail.com>
2024-01-22 15:32:13 -0700

    Add multiply function

---
+++ R/multiply.R
@@ -0,0 +1,15 @@
+#' Multiply two numbers
+#'
+#' This function takes two numbers and returns their product.
+#'
+#' @param x The first number
+#' @param y The second number
+#' @return The product of x and y
+#' @examples
+#' multiply(2, 3)
+#' # Returns: 6
+multiply <- function(x, y) {
+  result <- x * y
+  return(result)
+}
+
